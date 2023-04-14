14e6e18a625b53556402570af9857898b7655119
MGT1024 <gunter.tannehill@icloud.com>
2023-04-14 15:34:57 -0500

    new stuff

---
+++ BlackjackPi/Blackjack_BasicStrat.py
@@ -0,0 +1,34 @@
+from functools import reduce
+from basicStrategyDict import deckOfCards, hardTotalDict, softTotalDict, pairSplittingDict
+
+
+hand = [deckOfCards[None], deckOfCards[None]]
+dealerUpCard = None
+handTotal = reduce(lambda x, y: x+y, hand)
+
+
+
+if hand[0] == 11:
+    print(softTotalDict[hand[0]][dealerUpCard])
+
+elif hand[0] == hand[1]:
+    print(pairSplittingDict[hand[0]][dealerUpCard])
+
+else:
+    print(hardTotalDict[handTotal][dealerUpCard])
+
+
+def hardTotal():
+    hardTotalDict[None][None]
+
+def softTotal():
+    softTotalDict[None][None]
+
+def pairSplitting():
+    pairSplittingDict[None][None]
+
+
+Running = True
+
+while Running:
+    pass
